1eec2ea42aa52d5d1d8b5de9e02d3579da14de37
$Sudoerz <2509942930@qq.com>
2020-05-20 09:20:46 +0800

    wayne:Add dalvik-heap config

--- device.mk
+++ device.mk
@@ -35,6 +35,14 @@ PRODUCT_PACKAGES += \
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/wayne/wayne-vendor.mk)
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=8m \
+    dalvik.vm.heapmaxfree=32m \
+
 
 PRODUCT_PROPERTY_OVERRIDES += \
     touch.deviceType=touchScreen \
